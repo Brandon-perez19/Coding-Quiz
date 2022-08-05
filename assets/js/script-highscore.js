8f0b444caa64451512231f797a62d4d6e7c14576
Brandon <brandonperez@brandons-mbp.lan>
2022-08-05 15:06:26 -0400

    added styling to highscore page.

--- assets/js/script-highscore.js
+++ assets/js/script-highscore.js
@@ -3,12 +3,14 @@ function retrieveScores() {
     var display = document.querySelector("#placement");
     if (!highscores) {
         var list = document.createElement("li");
+        list.setAttribute("class", "list-group-item")
         list.textContent= "No Scores Saved!"
         display.appendChild(list);
     }
     else if (highscores.length >= 2) {
         for (let i = 0; i < highscores.length; i++) {
             var list = document.createElement("li");
+            list.setAttribute("class", "list-group-item")
             var finalScore = highscores[i].finalScore
             var initials = highscores[i].initials
             list.textContent = initials + " : " + finalScore
@@ -17,6 +19,7 @@ function retrieveScores() {
     }
     else {
         var list = document.createElement("li");
+        list.setAttribute("class", "list-group-item")
         var finalScore = highscores.finalScore
         var initials = highscores.initials
         list.textContent = initials + " : " + finalScore
